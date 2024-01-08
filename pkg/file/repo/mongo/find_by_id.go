20660a1adbada238b0f6b1364307bad9bb57477b
Kitajima 北島 <msg@kitajima.dev>
2024-01-07 21:41:01 -0300

    Optimization on File 'Find by ID': now both increment and fetch occurs concurrently

--- pkg/file/repo/mongo/find_by_id.go
+++ pkg/file/repo/mongo/find_by_id.go
@@ -2,14 +2,12 @@ package mongo
 
 import (
 	"context"
-	"log"
 
 	"upload/pkg/file"
 
 	"github.com/google/uuid"
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
-	"go.mongodb.org/mongo-driver/mongo"
 )
 
 func (db *FileCollection) FindByID(ctx context.Context, id uuid.UUID) (*file.File, error) {
@@ -21,26 +19,44 @@ func (db *FileCollection) FindByID(ctx context.Context, id uuid.UUID) (*file.Fil
 		Data:    []byte(id[:]),
 	}
 
-	_, err := db.UpdateByID(ctx, binID, bson.D{{Key: "$inc", Value: bson.D{{Key: "timesRequested", Value: 1}}}})
-	if err != nil {
-		return nil, err
+	incChan := make(chan error)
+	findChan := make(chan error)
+	go func() {
+		_, err := db.UpdateByID(ctx, binID, bson.D{{Key: "$inc", Value: bson.D{{Key: "timesRequested", Value: 1}}}})
+		incChan <- err
+	}()
+
+	go func() {
+		findChan <- db.FindOne(
+			ctx, bson.D{{
+				Key:   "_id",
+				Value: binID,
+			}},
+		).Decode(&f)
+	}()
+
+	select {
+	case err := <-incChan:
+		if err != nil {
+			return nil, err
+		}
+	case err := <-findChan:
+		if err != nil {
+			return nil, err
+		}
 	}
 
-	err = db.FindOne(
-		ctx, bson.D{{
-			Key:   "_id",
-			Value: binID,
-		}},
-	).Decode(&f)
-
-	if err != nil {
-		log.Println("failed to decode result")
-
-		if err == mongo.ErrNoDocuments {
-			log.Println("given filter did not match any docs on collection")
+	select {
+	case err := <-incChan:
+		if err != nil {
+			return nil, err
 		}
 
-		return nil, err
+		f.TimesRequested++
+	case err := <-findChan:
+		if err != nil {
+			return nil, err
+		}
 	}
 
 	return &f, nil
