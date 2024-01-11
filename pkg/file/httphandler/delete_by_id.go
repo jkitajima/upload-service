59022acacc20a7ed445443928fcfd2c6f2bfedf0
Kitajima 北島 <msg@kitajima.dev>
2024-01-11 01:49:55 -0300

    Updated error handling in File 'delete by id' operation

--- pkg/file/httphandler/delete_by_id.go
+++ pkg/file/httphandler/delete_by_id.go
@@ -5,6 +5,7 @@ import (
 	"net/http"
 	"upload/pkg/file"
 	"upload/util/blob"
+	"upload/util/encoding"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/google/uuid"
@@ -15,7 +16,8 @@ func (s *fileServer) handleFileDelete() http.HandlerFunc {
 		id := chi.URLParam(r, "fileID")
 		uuid, err := uuid.Parse(id)
 		if err != nil {
-			http.Error(w, "failed to parse uuid", http.StatusInternalServerError)
+			resp := NewErrorsResponse(&ErrorObject{http.StatusBadRequest, "Invalid Input", "Failed to parse UUID."})
+			encoding.Respond(w, r, resp, http.StatusBadRequest)
 			return
 		}
 
@@ -26,11 +28,13 @@ func (s *fileServer) handleFileDelete() http.HandlerFunc {
 		go func() { errChan <- file.Delete(ctx, s.db, uuid) }()
 		go func() { errChan <- blob.Delete(ctx, "company", uuid.String()) }()
 		if err := <-errChan; err != nil {
-			http.Error(w, "failed to delete requested file", http.StatusInternalServerError)
+			resp := NewErrorsResponse(&ErrorObject{http.StatusInternalServerError, "Internal Server Error", "Server encountered an unexpected condition that prevented it from fulfilling the request."})
+			encoding.Respond(w, r, resp, http.StatusInternalServerError)
 			return
 		}
 		if err := <-errChan; err != nil {
-			http.Error(w, "failed to delete requested file", http.StatusInternalServerError)
+			resp := NewErrorsResponse(&ErrorObject{http.StatusInternalServerError, "Internal Server Error", "Server encountered an unexpected condition that prevented it from fulfilling the request."})
+			encoding.Respond(w, r, resp, http.StatusInternalServerError)
 			return
 		}
 
