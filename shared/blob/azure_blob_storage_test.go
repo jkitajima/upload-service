4a694781f606f9ce3663dd837658a0fc46e5e5d8
Kitajima 北島 <msg@kitajima.dev>
2024-02-02 23:30:50 -0300

    Updated test for blob upload

--- shared/blob/azure_blob_storage_test.go
+++ shared/blob/azure_blob_storage_test.go
@@ -1,10 +1,21 @@
 package blob
 
 import (
+	"context"
+	"io"
+	"os"
+	"strings"
 	"testing"
+	"time"
+
+	"gocloud.dev/blob"
 )
 
 func TestNewAzureBlobStorage(t *testing.T) {
+	if os.Getenv("AZURE_STORAGE_ACCOUNT") == "" || os.Getenv("AZURE_STORAGE_KEY") == "" {
+		t.Skip("blob: azure_blob_storage: test_new_azure_blob_storage: required environment vars (`AZURE_STORAGE_ACCOUNT`, `AZURE_STORAGE_KEY`) not set. could not connect to a test blob storage")
+	}
+
 	cases := map[string]struct {
 		inSetenv  func(key, value string)
 		inAccount string
@@ -33,6 +44,39 @@ func TestNewAzureBlobStorage(t *testing.T) {
 			switch {
 			case blobstg.String() != testcase.outStg.String():
 				t.Errorf("blob: azure_blob_storage: test_new_azure_blob_storage: `Storager` interface mismatch (result = %q, expected = %q)\n", blobstg, testcase.outStg)
+			}
+		})
+	}
+}
+
+func TestUpload(t *testing.T) {
+	const timeout = 15 * time.Second
+	ctx, cancel := context.WithTimeout(context.Background(), timeout)
+	t.Cleanup(func() { cancel() })
+
+	blobstg, err := NewAzureBlobStorage()
+	if err != nil {
+		t.Skipf("blob: azure_blob_storage: test_new_azure_blob_storage: failed to create a blob storage for testing (err = %q)\n", err)
+	}
+
+	cases := map[string]struct {
+		inBucket, inKey string
+		inReader        io.Reader
+		inOpts          *blob.WriterOptions
+		outErr          error
+	}{
+		"basic upload": {"company", "file1.txt", strings.NewReader("file1 test data"), &blob.WriterOptions{ContentType: "text/plain"}, nil},
+		// "missing bucket":       {},
+		// "invalid bucket":       {},
+		// "missing key":          {},
+		// "nil reader":           {},
+		// "missing content-type": {},
+	}
+
+	for key, testcase := range cases {
+		t.Run(key, func(t *testing.T) {
+			if err := blobstg.Upload(ctx, testcase.inBucket, testcase.inKey, testcase.inReader, testcase.inOpts); err != testcase.outErr {
+				t.Errorf("blob: azure_blob_storage: test_new_azure_blob_storage: error mismatched (err = %q, expected = %q)\n", err, testcase.outErr)
 			}
 		})
 	}
